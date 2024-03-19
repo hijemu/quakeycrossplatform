34349cac30c9559c4de81a3dc307f768d98145a2
AnDayno <qamcdayno@tip.edu.ph>
2024-03-19 14:51:20 +0800

    quakey: Fix the bug on checklist

--- src/app/checklist/checklist.page.ts
+++ src/app/checklist/checklist.page.ts
@@ -46,53 +46,22 @@ export class ChecklistPage implements OnInit {
       }
     });
   }
-  
-  checklistClicked(event: MouseEvent): void {
-    const target = event.target as HTMLElement;
-    const threshold = 30;
-    const editButton = target.closest('.edit-button');
-    const checkbox = target.closest('ion-checkbox');
-    if (checkbox && Math.abs(checkbox.getBoundingClientRect().left - event.clientX) < threshold) {
-      event.stopPropagation();
-    }
-    if (editButton) {
-      event.stopPropagation();
-    }
-  }
 
+  addChecklist(checklistitem: string): void {
+    const newItem = { val: checklistitem, isChecked: false };
   
+    if (!this.checklists) {
+      this.checklists = [newItem];
+    } else {
+      this.checklists.push(newItem);
+    }
   
-
-  updateCheck(i: number): void {
-    this.storage.get('checklist').then(valueStr => {
-      let value = valueStr;
-      if (i == 0) {
-        if (value[i].isChecked) {
-          value[i].isChecked = false;
-          for (let count = 0; count < value[0].foods.length; count++) {
-            value[0].foods[count].isChecked = false;
-            this.foodcheckcount = 0;
-          }
-        } else {
-          value[i].isChecked = true;
-          for (let count = 0; count < value[0].foods.length; count++) {
-            value[0].foods[count].isChecked = true;
-            this.foodcheckcount = value[0].foods.length;
-          }
-        }
-      } else {
-        value[i].isChecked = !value[i].isChecked;
-      
-        for (let j = 0; j < value[i].foods.length; j++) {
-          value[i].foods[j].isChecked = value[i].isChecked;
-        }
-      }
-      this.storage.set('checklist', value);
-      this.checklists = value;
+    this.storage.set('checklist', this.checklists).then(() => {
+      console.log('Item Added!');
+    }).catch((error) => {
+      console.error('Error adding item: ', error);
     });
   }
-  
-
 
   addItem() {
     this.alertCtrl.create({
@@ -120,168 +89,8 @@ export class ChecklistPage implements OnInit {
       ]
     }).then(alert => alert.present());
   }
-  
 
-  addChecklist(checklistitem: string): void {
-    const newItem = { val: checklistitem, isChecked: false };
-  
-    if (!this.checklists) {
-      this.checklists = [newItem];
-    } else {
-      this.checklists.push(newItem);
-    }
-  
-    this.storage.set('checklist', this.checklists).then(() => {
-      console.log('Item Added!');
-    }).catch((error) => {
-      console.error('Error adding item: ', error);
-    });
-  }
-
-  addFood(index: number): void {
-    this.alertCtrl.create({
-      header: 'Add item',
-      inputs: [
-        {
-          name: 'moreItem',
-          type: 'text',
-          placeholder: 'Enter more item'
-        }
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        },
-        {
-          text: 'Add',
-          handler: (data) => {
-            if (data.moreItem.trim() !== '') {
-              this.addFoodSubmit(index, data.moreItem.trim());
-            }
-          }
-        }
-      ]
-    }).then(alert => alert.present());
-  }
-  
-  
-
-  async addFoodSubmit(index: number, moreItem: string): Promise<void> {
-    try {
-      if (!moreItem.trim()) {
-        return;
-      }
-  
-      const value = await this.storage.get('checklist') || [];
-  
-      if (!value[index].foods) {
-        value[index].foods = [];
-      }
-  
-      value[index].foods.push({ name: moreItem.trim(), isChecked: false });
-  
-      await this.storage.set('checklist', value);
-      this.checklists = value;
-  
-      
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
-
-  updateFoodCheck(i: number, j: number): void {
-    this.storage.get('checklist').then((valueStr: any[]) => {
-      let value = valueStr;
-      if (value[i].foods[j].isChecked) {
-        value[i].foods[j].isChecked = false;
-        value[i].isChecked = false;
-        this.foodcheckcount--;
-      } else {
-        value[i].foods[j].isChecked = true;
-        this.foodcheckcount++;
-        if (this.foodcheckcount === value[i].foods.length) {
-          value[i].isChecked = true;
-        }
-      }
-
-      const allFoodsChecked = value[i].foods.every((food: FoodItem) => food.isChecked);
-      value[i].isChecked = allFoodsChecked;
-
-      this.storage.set('checklist', value);
-      this.checklists = value;
-    });
-  }
-
-  deleteFoodConfirm(i: number, j: number): void {
-    let alert = this.alertCtrl.create({
-      header: 'Confirm Delete',
-      message: 'Are you sure you want to delete this item?',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Yes',
-          handler: () => {
-            this.deleteFoodItem(i, j);
-          }
-        }
-      ]
-    });
-    alert.then(alert => alert.present());
-  }
-  
-  
-
-  deleteFoodItem(i: number, j: number): void {
-    this.storage.get('checklist').then(valueStr => {
-      let value = valueStr;
-      value[i].foods.splice(j, 1);
-      this.storage.set('checklist', value);
-      this.checklists = value;
-    });
-  }
-
-  deleteConfirm(i: number): void {
-    let alert = this.alertCtrl.create({
-      header: 'Confirm Delete',
-      message: 'Are you sure you want to delete this checklist?',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Yes',
-          handler: () => {
-            this.deleteItem(i);
-          }
-        }
-      ]
-    });
-    alert.then(alert => alert.present());
-  }
-
-  deleteItem(i: number): void {
-    this.storage.get('checklist').then(valueStr => {
-      let value = valueStr;
-      value.splice(i, 1);
-      this.storage.set('checklist', value);
-      this.checklists = value;
-    });
-  }
-
-  editItem(event: MouseEvent, i: number): void {
-    event.stopPropagation();
+  editItem(i: number): void {
     this.storage.get('checklist').then(valueStr => {
       let value = valueStr;
       let alert1 = this.alertCtrl.create({
@@ -326,8 +135,146 @@ export class ChecklistPage implements OnInit {
     });
   }
 
-  editFood(event: MouseEvent, i: number, j: number): void {
-    event.stopPropagation(); 
+  updateCheck(i: number): void {
+    this.storage.get('checklist').then(valueStr => {
+      let value = valueStr;
+  
+      if (value[i].foods && value[i].foods.length > 0) {
+        if (i == 0) {
+          if (value[i].isChecked) {
+            value[i].isChecked = false;
+            for (let count = 0; count < value[0].foods.length; count++) {
+              value[0].foods[count].isChecked = false;
+              this.foodcheckcount = 0;
+            }
+          } else {
+            value[i].isChecked = true;
+            for (let count = 0; count < value[0].foods.length; count++) {
+              value[0].foods[count].isChecked = true;
+              this.foodcheckcount = value[0].foods.length;
+            }
+          }
+        } else {
+          value[i].isChecked = !value[i].isChecked;
+          for (let j = 0; j < value[i].foods.length; j++) {
+            value[i].foods[j].isChecked = value[i].isChecked;
+          }
+        }
+      } else {
+        value[i].isChecked = !value[i].isChecked;
+      }
+  
+      this.storage.set('checklist', value);
+      this.checklists = value;
+    });
+  }
+
+  deleteItem(i: number): void {
+    this.storage.get('checklist').then(valueStr => {
+      let value = valueStr;
+      value.splice(i, 1);
+      this.storage.set('checklist', value);
+      this.checklists = value;
+    });
+  }
+
+  deleteConfirm(i: number): void {
+    let alert = this.alertCtrl.create({
+      header: 'Confirm Delete',
+      message: 'Are you sure you want to delete this checklist?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.deleteItem(i);
+          }
+        }
+      ]
+    });
+    alert.then(alert => alert.present());
+  }
+  
+  addFood(index: number): void {
+    this.alertCtrl.create({
+      header: 'Add item',
+      inputs: [
+        {
+          name: 'moreItem',
+          type: 'text',
+          placeholder: 'Enter more item'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Add',
+          handler: (data) => {
+            if (data.moreItem.trim() !== '') {
+              this.addFoodSubmit(index, data.moreItem.trim());
+            }
+          }
+        }
+      ]
+    }).then(alert => alert.present());
+  }
+  
+  async addFoodSubmit(index: number, moreItem: string): Promise<void> {
+    try {
+      if (!moreItem.trim()) {
+        return;
+      }
+  
+      const value = await this.storage.get('checklist') || [];
+  
+      if (!value[index].foods) {
+        value[index].foods = [];
+      }
+  
+      value[index].foods.push({ name: moreItem.trim(), isChecked: false });
+  
+      await this.storage.set('checklist', value);
+      this.checklists = value;
+  
+      
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  updateFoodCheck(i: number, j: number): void {
+    this.storage.get('checklist').then((valueStr: any[]) => {
+      let value = valueStr;
+      if (value[i].foods[j].isChecked) {
+        value[i].foods[j].isChecked = false;
+        value[i].isChecked = false;
+        this.foodcheckcount--;
+      } else {
+        value[i].foods[j].isChecked = true;
+        this.foodcheckcount++;
+        if (this.foodcheckcount === value[i].foods.length) {
+          value[i].isChecked = true;
+        }
+      }
+
+      const allFoodsChecked = value[i].foods.every((food: FoodItem) => food.isChecked);
+      value[i].isChecked = allFoodsChecked;
+
+      this.storage.set('checklist', value);
+      this.checklists = value;
+    });
+  }
+
+  editFood(i: number, j: number): void {
     this.storage.get('checklist').then(valueStr => {
       let value = valueStr;
       const alert1 = this.alertCtrl.create({
@@ -377,5 +324,36 @@ export class ChecklistPage implements OnInit {
       console.error(error);
     }
   }
-  
+
+  deleteFoodItem(i: number, j: number): void {
+    this.storage.get('checklist').then(valueStr => {
+      let value = valueStr;
+      value[i].foods.splice(j, 1);
+      this.storage.set('checklist', value);
+      this.checklists = value;
+    });
+  }
+
+  deleteFoodConfirm(i: number, j: number): void {
+    let alert = this.alertCtrl.create({
+      header: 'Confirm Delete',
+      message: 'Are you sure you want to delete this item?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.deleteFoodItem(i, j);
+          }
+        }
+      ]
+    });
+    alert.then(alert => alert.present());
+  }
 }
